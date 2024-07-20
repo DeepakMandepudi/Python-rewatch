eea89c90b040d479b0a1e17cd3740cd5e680e38c
DeepakMandepudi <67830264+DeepakMandepudi@users.noreply.github.com>
2024-07-20 16:19:54 -0500

    Add files via upload

---
+++ InputTests.py
@@ -0,0 +1,14 @@
+
+hey = "hello"
+name =input("enter the name: ")
+print(hey ,name)
+print(hey+name)
+print(hey, 'this is', name)
+
+letter = len(input())
+print(letter)
+word = len('input()')
+print(word)
+
+input('enter your name')
+print("hello" , input('enter your name: '))

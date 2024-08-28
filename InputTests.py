f8ac25eac846f6dc3e750d9d52bf63ed17b8344f
Deepak Kumar Mandepudi <67830264+DeepakMandepudi@users.noreply.github.com>
2024-08-28 15:28:46 -0500

    Input methods with example

--- InputTests.py
+++ InputTests.py
@@ -1,14 +1,13 @@
 
 hey = "hello"
-name =input("enter the name: ")
-print(hey ,name)
-print(hey+name)
-print(hey, 'this is', name)
+name =input("enter the name: ")   #enter the name: deepak
+print(hey ,name)    #print: hello deepak
+print(hey+name)   #print: hellodeepak
+print(hey, 'this is', name)   #hello this is deepak
 
-letter = len(input())
-print(letter)
-word = len('input()')
-print(word)
+letter = len(input())   #input = deepak
+print(letter)   #prints length of deepak = 6
+word = len('input()')   #here it is not an input function. 'input()' is a string. 
+print(word)   #length of "input()" = 7 [ I, n, p, u, t, (, ) ]
 
-input('enter your name')
-print("hello" , input('enter your name: '))
+print("hello" , input('enter your name: '))   #enter your name: deepak. prints: hello deepak
